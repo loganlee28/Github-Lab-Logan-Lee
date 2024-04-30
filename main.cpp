4cb93c3c9da598add547cdf720b7449c6033aebd
loganlee28 <167340997+loganlee28@users.noreply.github.com>
2024-04-30 18:08:09 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,12 +6,22 @@ using namespace std;
 
 void PrintMenu() {
    /* Type your code here */
+   cout << "MENU" << endl;
+   cout << "a - Add item to cart" << endl;
+   cout << "d - Add item to cart" << endl;
+   cout << "c - Add item to cart" << endl;
+   cout << "i - Add item to cart" << endl;
+   cout << "o - Add item to cart" << endl;
+   cout << "q - Add item to cart" << endl;
    
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
    /* Type your code here */
-   
+   switch (option) {
+      case 'o':
+      cout << "OUTPUT SHOPPING CART" << endl
+   }
 }
 
 int main() {
